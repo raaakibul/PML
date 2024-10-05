baa5b067881ee30d2df2e6b6105421f7272f2bcd
rakibul-islam <raakibul.uap@gmail.com>
2024-10-05 22:59:36 +0600

    machine learning model

--- linear-regression.py
+++ linear-regression.py
@@ -9,6 +9,8 @@ scores = np.array([56,83,47,93,47,82,45,78,55,67,57,4,12]).reshape(-1,1)
 model = LinearRegression()
 model.fit(time_studied, scores)
 
+print(model.predict(np.array([56]).reshape(-1,1)))
+
 plt.scatter(time_studied, scores)
 plt.plot(np.linspace(0,70,100).reshape(-1,1), model.predict(np.linspace(0,70,100).reshape(-1,1)), 'r')
 plt.ylim(0,100)
