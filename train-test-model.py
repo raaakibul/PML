9c55794fcaf60aa7625d880a5f0620e6b8fb8287
rakibul-islam <raakibul.uap@gmail.com>
2024-10-06 22:49:56 +0600

    train test model

--- train-test-model.py
+++ train-test-model.py
@@ -8,3 +8,10 @@ scores = np.array([56,83,47,93,47,82,45,78,55,67,57,4,12]).reshape(-1,1)
 
 time_train, time_test, score_train, score_test = train_test_split(time_studied,scores, test_size=0.2)
 
+model = LinearRegression()
+model.fit(time_train, score_train,)
+
+print(model.score(time_test, score_test))
+
+plt.scatter(time_train,score_train)
+plt.show()
