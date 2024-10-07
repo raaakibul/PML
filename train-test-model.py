3a2777b23d927629d4506e8c3aa825a73e7a8332
rakibul-islam <raakibul.uap@gmail.com>
2024-10-07 23:15:19 +0600

    train test model

--- train-test-model.py
+++ train-test-model.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 time_studied = np.array([20,50,32,65,23,43,10,5,22,35,29,5,56]).reshape(-1,1)
 scores = np.array([56,83,47,93,47,82,45,78,55,67,57,4,12]).reshape(-1,1)
 
-time_train, time_test, score_train, score_test = train_test_split(time_studied,scores, test_size=0.2)
+time_train, time_test, score_train, score_test = train_test_split(time_studied,scores, test_size=0.1)
 
 model = LinearRegression()
 model.fit(time_train, score_train,)
@@ -14,4 +14,5 @@ model.fit(time_train, score_train,)
 print(model.score(time_test, score_test))
 
 plt.scatter(time_train,score_train)
+plt.plot(np.linspace(0,70,100).reshape(-1,1), model.predict(np.linspace(0,70,100).reshape(-1,1)))
 plt.show()
