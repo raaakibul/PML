0774e94355566afac48e2d719366fabb25fe9611
rakibul-islam <raakibul.uap@gmail.com>
2024-10-10 23:23:37 +0600

    svm

--- svm.py
+++ svm.py
@@ -10,4 +10,7 @@ y = data.target
 
 x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2)
 
-clf = SVC(kernel='linear')
+clf = SVC(kernel='linear', C=3)
+
+clf.fit(x_train, y_train)
+print(clf.score(x_test, y_test))
