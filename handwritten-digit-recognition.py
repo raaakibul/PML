4ac9d6954b1cad82421e1dec9388b2df10b49606
rakibul-islam <raakibul.uap@gmail.com>
2024-10-15 23:04:25 +0600

    handwritten recognition

--- handwritten-digit-recognition.py
+++ handwritten-digit-recognition.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt 
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist
@@ -11,9 +13,9 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.mn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.mn.relu))
-model.add(tf.keras.layers.Dense(units=10, activation=tf.mn.softmax))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
