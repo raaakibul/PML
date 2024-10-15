e1531dc19b2db7131e6d183e5db00a6ff5893c30
rakibul-islam <raakibul.uap@gmail.com>
2024-10-15 22:54:11 +0600

    handwritten recognition

--- handwritten-digit-recognition.py
+++ handwritten-digit-recognition.py
@@ -12,4 +12,16 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
 model.add(tf.keras.layers.Dense(units=128, activation=tf.mn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation=tf.mn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation=tf.mn.softmax))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+
+accuracy, loss = model.evaluate(x_test, y_test)
+print(accuracy)
+print(loss)
+
+model.save('digits.model')
 
